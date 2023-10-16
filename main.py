6bd56e62d4c511735a5c95dd5245b834c2f8362e
prajwal-br-03 <81077507+prajwal-br-03@users.noreply.github.com>
2023-10-16 22:22:38 +0530

    new commit

--- main.py
+++ main.py
@@ -1,33 +1,105 @@
-import numpy as np
-import requests
 from bs4 import BeautifulSoup
+import requests
 import pandas as pd
-url="https://www.amazon.in/s?k=bags&crid=2M096C61O4MLT&qid=1653308124&sprefix=ba%2Caps%2C283&ref=sr_pg_1"
-HEADERS=({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36','Accept-Language':'en-US,en;q=0.5'})
-webpage = requests.get(url,headers=HEADERS)
+import numpy as np
 
-soup = BeautifulSoup(webpage.content,"html.parser")
-link = soup.find_all("span",attrs={'class':'a-size-medium a-color-base a-text-normal'})
 
-num_pages_to_scrape = 20
+def get_title(soup):
+    try:
 
-d={"product_name":[],"price":[],"rating":[],"reviews":[]}
+        title = soup.find("span", attrs={"id": 'productTitle'})
 
-for page_num in range(1, num_pages_to_scrape + 1):
-    page_url = f"{webpage}{page_num}"
+
+        title_value = title.text
+
+
+        title_string = title_value.strip()
+
+    except AttributeError:
+        title_string = ""
+
+    return title_string
+
+
+
+def get_price(soup):
+    try:
+        price = soup.find("span", attrs={'id': 'priceblock_ourprice'}).string.strip()
+
+    except AttributeError:
+
+        try:
+
+            price = soup.find("span", attrs={'id': 'priceblock_dealprice'}).string.strip()
+
+        except:
+            price = ""
+
+    return price
+
+
+def get_rating(soup):
+    try:
+        rating = soup.find("i", attrs={'class': 'a-icon a-icon-star a-star-4-5'}).string.strip()
+
+    except AttributeError:
+        try:
+            rating = soup.find("span", attrs={'class': 'a-icon-alt'}).string.strip()
+        except:
+            rating = ""
+
+    return rating
+
+
+def get_review_count(soup):
+    try:
+        review_count = soup.find("span", attrs={'id': 'acrCustomerReviewText'}).string.strip()
+
+    except AttributeError:
+        review_count = ""
+
+    return review_count
+
+
+
+if __name__ == '__main__':
+
+    URL = "https://www.amazon.in/s?k=bags&crid=2M096C61O4MLT&qid=1653308124&sprefix=ba%2Caps%2C283&ref=sr_pg_1"
+    HEADERS = ({
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.5'})
+
+
+    webpage = requests.get(URL, headers=HEADERS)
+
+
     soup = BeautifulSoup(webpage.content, "html.parser")
-    link = soup.find_all("span", attrs={'class': 'a-size-medium a-color-base a-text-normal'})
-    price=soup.find_all("span",attrs={'class':'a-price-whole'})
-    rating=soup.find_all("span",attrs={'class':'a-size-base s-underline-text'})
-    review=soup.find_all("span",attrs={'class':'a-icon-alt'})
-    for i in range(0,len(link)):
-        d["product_name"].append(link[i].text.strip())
-        d["price"].append(price[i].text.strip())
-        #d["reviews"].append(rating[i].text.strip())
-        d["rating"].append(review[i].text.strip())
 
-amazon_df=pd.DataFrame.from_dict(d)
-amazon_df['product_name'].replace('',np.nan,inplace=True)
-amazon_df=amazon_df.dropna(subset=['product_name'])
-amazon_df.to_csv("amazon_data.csv",header=True,index=False)
-print(amazon_df)
+
+    links = soup.find_all("a", attrs={'class': 'a-link-normal s-no-outline'})
+
+
+    links_list = []
+
+
+    for link in links:
+        links_list.append(link.get('href'))
+
+    d = {"title": [], "price": [], "rating": [], "reviews": []}
+
+
+    for link in links_list:
+        new_webpage = requests.get("https://www.amazon.com" + link, headers=HEADERS)
+
+        new_soup = BeautifulSoup(new_webpage.content, "html.parser")
+
+
+        d['title'].append(get_title(new_soup))
+        d['price'].append(get_price(new_soup))
+        d['rating'].append(get_rating(new_soup))
+        d['reviews'].append(get_review_count(new_soup))
+
+    amazon_df = pd.DataFrame.from_dict(d)
+    amazon_df['title'].replace('', np.nan, inplace=True)
+    amazon_df = amazon_df.dropna(subset=['title'])
+    amazon_df.to_csv("amazon_data.csv", header=True, index=False)
